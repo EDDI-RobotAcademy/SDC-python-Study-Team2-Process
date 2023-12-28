e257305fb3a6df5b1c5d92dd6bd7f4108ccf94f8
LoHenP <44047542+LoHenP@users.noreply.github.com>
2023-12-28 10:48:43 +0900

    update: 더 편하게 변경

--- Process/tests/receive/repository/TestReceiveRepository.py
+++ Process/tests/receive/repository/TestReceiveRepository.py
@@ -16,7 +16,7 @@ class TestReceiveRepository(unittest.TestCase):
 
     def testReceive(self):
         # receivedForm = {
-        #     "protocol": 1,
+        #     "protocol": 2,
         #     "data": {
         #         "__accountId": "test_user",
         #         "__password": "password"
@@ -24,9 +24,11 @@ class TestReceiveRepository(unittest.TestCase):
         # }
 
         receivedForm = {
-            "protocol": 5,
+            "protocol": 7,
             "data": {
-
+                "name": "test",
+                "price": 22222,
+                "info": "wrqwe"
 
             }
         }
