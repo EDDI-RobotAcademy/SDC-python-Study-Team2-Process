895646940ca929fdb62bef7b956f3a4a6faad1ab
LoHenP <qwpoal119@naver.com>
2023-12-28 15:44:47 +0900

    feat: [이재승] response를 transmit 할 수 있는 데이터로 변환 [PTAN없음]

--- Process/product/service/response/ProductResponseList.py
+++ Process/product/service/response/ProductResponseList.py
@@ -12,4 +12,17 @@ class ProductResponseList:
         self.__name = name
         self.__price = price
 
+    def __iter__(self):
+        # 객체를 iterable로 만들기 위해 __iter__ 메서드를 정의
+        yield "id", self.__id
+        yield "name", self.__name
+        yield "price", self.__price
+
+    def __dict__(self):
+        return {
+            "id": self.__id,
+            "name": self.__name,
+            "price": self.__price
+        }
+
 
